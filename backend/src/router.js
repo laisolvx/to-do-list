f96f7e6f32f832c2e927afd2614543b6c24eed8f
laisolvx <laissolv08@gmail.com>
2023-03-30 14:38:19 -0300

    added cors

---
+++ backend/src/router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+const tasksController = require('./controllers/tasksController');
+const tasksMiddlewares = require('./middlewares/tasksMiddlewares');
+
+
+router.get('/tasks', tasksController.getAll);
+router.post('/tasks', tasksMiddlewares.validateFieldTitle, tasksController.createTask);
+router.delete('/tasks/:id', tasksController.deleteTask);
+router.put('/tasks/:id',
+    tasksMiddlewares.validateFieldTitle,
+    tasksMiddlewares.validateFieldStatus,
+    tasksController.updateTask);
+
+
+module.exports = router;
